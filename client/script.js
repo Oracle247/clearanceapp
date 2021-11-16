e7c2ce571400d6a6c9f940f8ffab823364c50b8e
Oracle247 <94334497+Oracle247@users.noreply.github.com>
2021-11-16 18:47:21 +0100

    add responsive design

--- client/script.js
+++ client/script.js
@@ -1,6 +1,19 @@
 console.log("hello")
 
-function click(){
-    var ticket = document.getElementById("ticket");
-    ticket.classList.toggle('active');     
+function click() {
+  var ticket = document.getElementById("ticket");
+  ticket.classList.toggle('active');
 }
+
+$(document).ready( function () {
+  $('#table_id').DataTable({
+    rowReorder: true,
+    // scrollX: 300,
+    // scrollY: '200px',
+    // scrollCollapse: true,
+    "lengthMenu": [[2, 5, 10, -1], [2, 5, 10, "All"]]
+    // responsive: true,
+    // paging: false
+
+  });
+});
